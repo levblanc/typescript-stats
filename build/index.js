fb961d6c8bdd18a2b77efa7bf13d97ee169dce72
levblanc <levblanc@gmail.com>
2022-08-09 17:00:24 +0800

    Refactor with interface approach

--- build/index.js
+++ build/index.js
@@ -1,6 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const reader = new MatchReader_1.MatchReader('football.csv');
-reader.read();
-console.log(reader.data[0][0]);
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_1 = require("./MatchResult");
+// Inheritance approach
+// const reader = new MatchReader('football.csv')
+// reader.read()
+// console.log(reader.data[0][0])
+// Interface approach
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+// Create an instance of MatchReader and pass in something satisfying 
+// the 'DataReader' interface
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+let manUnitedWins = 0;
+for (let match of matchReader.matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin)
+        manUnitedWins++;
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
 import { MatchReader } from "./MatchReader";
-const reader = new MatchReader('football.csv')
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
-reader.read()
-console.log(reader.data[0][0])
+// Inheritance approach
+// const reader = new MatchReader('football.csv')
+// reader.read()
+// console.log(reader.data[0][0])
+
+// Interface approach
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv')
+
+// Create an instance of MatchReader and pass in something satisfying 
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load()
+
+let manUnitedWins = 0;
+
+for (let match of matchReader.matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
+    manUnitedWins++;
+}
+
+console.log(`Man United won ${manUnitedWins} games`)
